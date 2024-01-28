1ffa41e3edef4109706da5f9ef7c6e91e78806ee
yanickflyer <yanick76@live.com>
2024-01-28 23:24:24 +0400

    call buy quota function

--- src/launch.py
+++ src/launch.py
@@ -4,6 +4,7 @@ class launch:
     def fuel():
         while True:
             functions.actions.purchase_fuel(1)
+            functions.actions.buy_quota(1)
             time.sleep(1800)
     
     def depart_maintenance():
