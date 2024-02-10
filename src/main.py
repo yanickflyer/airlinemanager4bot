06a819d34b72f52bdc1114dc9d6155afa53ce153
yanickflyer <yanick76@live.com>
2024-02-10 21:00:07 +0400

    update git ignore requirement.txt

--- src/main.py
+++ src/main.py
@@ -1,10 +1,18 @@
 import launch, threading
+from apscheduler.schedulers.background import BackgroundScheduler
+from marketing import marketing
+
 
 thread1=threading.Thread(target=launch.launch.depart_maintenance)
 thread2=threading.Thread(target=launch.launch.fuel)
 
 thread1.start()
 thread2.start()
+
+scheduler = BackgroundScheduler()
+job = scheduler.add_job(marketing.eco_campaign,'cron', hours=7, minute=0, timezone="Indian/Mauritius")
+scheduler.start()
+
 
 while True:
     pass
