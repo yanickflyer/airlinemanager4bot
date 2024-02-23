17435df6e6c13ece77fdb1dcf68b92dbae65b2b9
yanickflyer <yanick76@live.com>
2024-02-23 10:36:26 +0400

    normall http request in a function apply it to marketing first

--- src/marketing.py
+++ src/marketing.py
@@ -1,75 +1,28 @@
 import requests, re
 from session import auth
 from bs4 import BeautifulSoup
+from am4_req import am4_url
 
 class marketing:
     def eco_campaign():
-        try:
-            parameter={
+        parameter={
                     "type":"5"
-            }
-            resp=requests.get(auth.url+"marketing_new.php", cookies=auth.session,params=parameter)
-        except requests.exceptions.HTTPError as errh:
-            print("Connection Lost for Marketing. Retrying")
-            print("HTTP Error") 
-            print(errh.args[0])
-            marketing.eco_campaign()
-            return
-        except requests.exceptions.ReadTimeout as errrt:
-            print("Connection Lost for Marketing. Retrying")
-            print("Time out")
-            print(errrt)
-            marketing.eco_campaign()
-            return
-        except requests.exceptions.ConnectionError as conerr:
-            print("Connection Lost for Marketing. Retrying")
-            print("Connection error")
-            print(conerr)
-            marketing.eco_campaign()
-            return
-        except requests.exceptions.RequestException as errex:
-            print("Connection Lost for Marketing. Retrying")
-            print("Exception request")
-            print(errex)
-            marketing.eco_campaign()
-            return
-        
+        }
+        resp = am4_url.send_req("marketing_new.php",parameter=parameter)
         html_read=BeautifulSoup(resp.text,'html.parser')
         check_active = html_read.find().string
         check_active = re.search(r'You already have an active campaign',check_active)
         if check_active:
             print("Eco Campaign is already Active")
+            resp = am4_url.send_req("marketing.php",parameter={})
+            html_read=BeautifulSoup(resp.text,'html.parser')
+            active = html_read.find("div",{"id": "active-campaigns"})
         else:
             eco_price=html_read.find("button").string
-            try:
-                parameter={
+            parameter={
                         "type":"5",
                         "mode":"do",
                         "c":"1"
-                }
-                resp=requests.post(auth.url+"marketing_new.php", cookies=auth.session,params=parameter)
-                print("Eco Campaign Bought for "+eco_price)
-            except requests.exceptions.HTTPError as errh:
-                print("Connection Lost for Marketing. Retrying")
-                print("HTTP Error") 
-                print(errh.args[0])
-                marketing.eco_campaign()
-                return
-            except requests.exceptions.ReadTimeout as errrt:
-                print("Connection Lost for Marketing. Retrying")
-                print("Time out")
-                print(errrt)
-                marketing.eco_campaign()
-                return
-            except requests.exceptions.ConnectionError as conerr:
-                print("Connection Lost for Marketing. Retrying")
-                print("Connection error")
-                print(conerr)
-                marketing.eco_campaign()
-                return
-            except requests.exceptions.RequestException as errex:
-                print("Connection Lost for Marketing. Retrying")
-                print("Exception request")
-                print(errex)
-                marketing.eco_campaign()
-                return
+            }
+            resp = am4_url.send_req("marketing_new.php",parameter=parameter)
+            print("Eco Campaign Bought for "+eco_price)
